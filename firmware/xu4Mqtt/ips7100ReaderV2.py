be0b3c3c7be94f59db77fa0d17f42fedf7940632
lakithaomal <lhw150030@utdallas.edu>
2023-02-06 22:47:32 +0000

    dailyUpdates

--- firmware/xu4Mqtt/ips7100ReaderV2.py
+++ firmware/xu4Mqtt/ips7100ReaderV2.py
@@ -6,46 +6,48 @@ from mintsXU4 import mintsDefinitions as mD
 import sys
 
 dataFolder  = mD.dataFolder
-ipsPort     = mD.ipsPorts[0]
+ipsPorts     = mD.ipsPorts
 baudRate    = 115200
 
-def main():
-    ser = serial.Serial(
-    port= ipsPort,\
-    baudrate=baudRate,\
-    parity  =serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-    timeout=0)
+def main(portNum):
+    if(len(ipsPorts)>0):
+        ser = serial.Serial(
+        port= ipsPorts[portNum],\
+        baudrate=baudRate,\
+        parity  =serial.PARITY_NONE,\
+        stopbits=serial.STOPBITS_ONE,\
+        bytesize=serial.EIGHTBITS,\
+        timeout=0)
 
-    print(" ")
-    print("Connected to: " + ser.portstr)
-    print(" ")
+        print(" ")
+        print("Connected to: " + ser.portstr)
+        print(" ")
 
         #this will store the line
-    line = []
+        line = []
 
-    while True:
-        try:
-            for c in ser.read():
-#                print(chr(c))
-                line.append(chr(c))
-                if chr(c) == '\n':
-                    dataString     = (''.join(line))
-                    dataStringPost = dataString.replace('\n', '')
-                    print("================")
-                    mSR.IPS7100Write(dataStringPost,datetime.datetime.now())
-                    line = []
-                    break
-        except OSError as e:
-            print ("Error: %s - %s." % (e.filename, e.strerror))
-            print("Microphone Not Connected: Check connection")
-    ser.close()
-
+        while True:
+            try:
+                for c in ser.read():
+#                   print(chr(c))
+                    line.append(chr(c))
+                    if chr(c) == '\n':
+                        dataString     = (''.join(line))
+                        dataStringPost = dataString.replace('\n', '')
+                        print("================")
+                        mSR.IPS7100Write(dataStringPost,datetime.datetime.now())
+                        line = []
+                        break
+            except OSError as e:
+                print ("Error: %s - %s." % (e.filename, e.strerror))
+                print("Microphone Not Connected: Check connection")
+        ser.close()
 
 if __name__ == "__main__":
     print("=============")
     print("    MINTS    ")
     print("=============")
-    print("Monitoring IPS7100 on port: {0}".format(ipsPort) + " with baudrate " + str(baudRate))
-    main()    
+    portNum = int(sys.argv[1])
+    print("Number of IPS7100 devices: {0}".format(len(ipsPorts)))
+    print("Monitoring IPS7100 on port: {0}".format(ipsPorts[portNum]) + " with baudrate " + str(baudRate))
+    main(portNum) 
